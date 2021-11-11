4e002364e2920440e24178a40ef68badee766289
beritcarvalho <56369664+beritcarvalho@users.noreply.github.com>
2021-11-10 22:07:15 -0300

    Criações de Tabelas

---
+++ SQLQuery.sql
@@ -0,0 +1,97 @@
+--CREATE DATABASE [berit]
+
+USE berit
+
+CREATE TABLE [Student]
+(
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [Name] NVARCHAR(120) NOT NULL,
+    [Email] NVARCHAR(180) NOT NULL,
+    [Document] NVARCHAR(20) NULL,
+    [Phone] NVARCHAR(20) NULL,
+    [Birthdate] DATETIME NULL,
+    [CreateDate] DATETIME NOT NULL DEFAULT
+    (GETDATE()),
+    CONSTRAINT [PK_Student] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [Author]
+(
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [Name] NVARCHAR(80) NOT NULL,
+    [Title] NVARCHAR(80) NOT NULL,
+    [Image] NVARCHAR(1024) NOT NULL,
+    [Bio] NVARCHAR(2000) NOT NULL,
+    [Url] NVARCHAR(450) NULL,
+    [Email] NVARCHAR(160) NOT NULL,
+    [Type] TINYINT NOT NULL,
+    CONSTRAINT [Pk_Author] PRIMARY KEY ([Id])
+);
+GO
+
+SELECT * from Student;
+
+CREATE TABLE [Career]
+(
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [Title] NVARCHAR(160) NOT NULL,
+    [Summary] NVARCHAR(2000) NOT NULL,
+    [Url] NVARCHAR(1024) NOT NULL,
+    [DurationInMinutes] INT NOT NULL,
+    [Active] BIT NOT NULL,
+    [Featured] BIT NOT NULL,
+    [Tags] NVARCHAR(160) NOT NULL,
+    CONSTRAINT [PK_Career] PRIMARY KEY([Id])
+);
+GO
+
+SELECT * from Career;
+
+CREATE TABLE [Category]
+(
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [Title] NVARCHAR(160) NOT NULL,
+    [Url] NVARCHAR(1024) NOT NULL,
+    [Summary] NVARCHAR(2000) NOT NULL,
+    [Order] INT NOT NULL,
+    [Description] TEXT NOT NULL,
+    [Featured] BIT NOT NULL,
+    CONSTRAINT [PK_Categoria] PRIMARY KEY ([Id])
+);
+GO
+
+SELECT * FROM Category;
+
+CREATE TABLE [Course]
+(
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [Tag] NVARCHAR(20) NOT NULL,
+    [Title] NVARCHAR(160) NOT NULL,
+    [Summary] NVARCHAR (2000) NOT NULL,
+    [Url] NVARCHAR(1024) NOT NULL,
+    [Level] TINYINT NOT NULL,
+    [DurationInMinutes] INT NOT NULL,
+    [CreateDate] DATETIME NOT NULL,
+    [LastUpdateDate] DATETIME NOT NULL,
+    [Active] BIT NOT NULL,
+    [Free] BIT NOT NULL,
+    [Featured] BIT NOT NULL,
+    [AuthorId] UNIQUEIDENTIFIER NOT NULL,
+    [CategoryId] UNIQUEIDENTIFIER NOT NULL,
+    [Tags] NVARCHAR(160) NOT NULL,
+    CONSTRAINT [PK_Course] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Course_Author_Author_Id] FOREIGN KEY ([AuthorId])
+        REFERENCES [Author] ([Id])
+            ON DELETE NO ACTION,
+    CONSTRAINT [FK_Couse_Category_CategoryId] FOREIGN KEY ([CategoryId])
+        REFERENCES [Category] ([Id])
+            ON DELETE NO ACTION   
+);
+GO  
+
+SELECT * FROM Course;
+
+
+
+--DROP TABLE Course;
